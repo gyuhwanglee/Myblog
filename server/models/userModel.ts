dd5e68f82c442d32d040a337040dd3d2527ec876
gyuhwanglee <el@Appleui-MacBookAir.local>
2021-12-29 18:31:07 +0900

    controllers middleware models routes config

---
+++ server/models/userModel.ts
@@ -0,0 +1,40 @@
+
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please add your name"],
+    trim: true,
+    maxLength: [20, "Your name is up to 20 chars long."]
+  },
+  account: {
+    type: String,
+    required: [true, "Please add your email or phone"],
+    trim: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: [true, "Please add your password"],
+    trim: true
+  },
+  avatar: {
+    type: String,
+    default : 'https://thumbs.dreamstime.com/z/default-avatar-profile-icon-social-media-user-vector-default-avatar-profile-icon-social-media-user-vector-portrait-176194876.jpg'
+
+    // default: 'https://res.cloudinary.com/devatchannel/image/upload/v1602752402/avatar/avatar_cugq40.png'
+  },
+  role: {
+    type: String,
+    defaul: 'user' // admin
+  },
+  type: {
+    type: String,
+    defaul: 'normal' // fast
+  }
+}, {
+  timestamps: true
+})
+
+export default mongoose.model('User', userSchema)
